edbce73736d9dcdb1a2f51dc51a84583fbcd7d65
Sal-Y <syouhanna@goodleap.com>
2024-09-10 14:47:31 -0400

    more ios configs

--- ios/RunnerUITests/RunnerUITests.m
+++ ios/RunnerUITests/RunnerUITests.m
@@ -1,46 +1,5 @@
-//
-//  RunnerUITests.m
-//  RunnerUITests
-//
-//  Created by Sal Youhanna on 2024-09-10.
-//
+@import XCTest;
+@import patrol;
+@import ObjectiveC.runtime;
 
-#import <XCTest/XCTest.h>
-
-@interface RunnerUITests : XCTestCase
-
-@end
-
-@implementation RunnerUITests
-
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // UI tests must launch the application that they test.
-    XCUIApplication *app = [[XCUIApplication alloc] init];
-    [app launch];
-
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testLaunchPerformance {
-    if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *)) {
-        // This measures how long it takes to launch your application.
-        [self measureWithMetrics:@[[[XCTApplicationLaunchMetric alloc] init]] block:^{
-            [[[XCUIApplication alloc] init] launch];
-        }];
-    }
-}
-
-@end
+PATROL_INTEGRATION_TEST_IOS_RUNNER(RunnerUITests)
